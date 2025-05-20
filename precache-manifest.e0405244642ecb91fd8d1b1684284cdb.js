bd5645fe2480350af6436d9d42c0b644e0c82589
Daniil <shapovalov.pr@gmail.com>
2025-05-20 11:03:12 +0200

    Updates

---
+++ precache-manifest.e0405244642ecb91fd8d1b1684284cdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7733c54092a77de640346c11ed993966",
+    "url": "/Redux-practice/index.html"
+  },
+  {
+    "revision": "41e5ed9bf1e1866e9ddd",
+    "url": "/Redux-practice/static/css/main.8e6c22e8.chunk.css"
+  },
+  {
+    "revision": "df4d495e1d149c5a887d",
+    "url": "/Redux-practice/static/js/2.e5411fe2.chunk.js"
+  },
+  {
+    "revision": "b19ce14f6f492849f07b572a02dc0e95",
+    "url": "/Redux-practice/static/js/2.e5411fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41e5ed9bf1e1866e9ddd",
+    "url": "/Redux-practice/static/js/main.f98a0758.chunk.js"
+  },
+  {
+    "revision": "fec1ade57c3bef281d5b",
+    "url": "/Redux-practice/static/js/runtime-main.2ed8fc84.js"
+  }
+]);
